d98a5480b743f64181ed5c6cd3f650465987e3c5
Milad Ghalejughi <milad@amaka.com>
2022-08-04 13:45:46 +1000

    editing the app file for custom view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
     <title>@yield('title', 'Online Store')</title>
 </head>
 <body>
@@ -32,6 +33,18 @@
 <div class="container my-4">
     @yield('content')
 </div>
+<!-- footer -->
+<div class="copyright py-4 text-center text-white">
+    <div class="container">
+        <small>
+            Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
+                           href="https://twitter.com/ghalejughi">
+                Milad Ghalejughi
+            </a>
+        </small>
+    </div>
+</div>
+<!-- footer -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
 </script>
 </body>
